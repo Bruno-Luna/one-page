34b523e6dbd1b22db52778aac66e4b571cc6ffa1
Bruno Luna <luna.brsilva@gmail.com>
2021-08-03 20:09:23 -0300

    criando a biblioteca ScrollReveal

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+origin: 'top',
+distance: '50px',
+duration: 1000,
+reset: true
+});
+
+ScrollReveal().reveal('.conteudo-topo', { delay: 200 });
+ScrollReveal().reveal('.card', { delay: 300 });
+ScrollReveal().reveal('.formulario', { delay: 400 });
+
